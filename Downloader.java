80a7bf16690276002b81b66f07a83fad61e0ac3b
Miguel Meneses <menesesm133@gmail.com>
2025-03-04 21:31:36 +0000

    Setting up the jsoup library

--- Downloader.java
+++ Downloader.java
@@ -1,5 +1,14 @@
 package SD_Project;
 
-public class Downloader {
+import org.json.JSONObject;
+
+public class Downloader extends Thread{
+    public Downloader() {
+        super("Server: " + (long) (Math.random() * 100));
+    }
+    
+    private JSONObject searchURL(String url) {
+
+    }
     
 }
