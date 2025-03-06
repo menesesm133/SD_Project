0b12a505eb687308011083050eeff9410f89a40b
Miguel Meneses <menesesm133@gmail.com>
2025-03-06 13:48:10 +0000

    Gets the urls

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,6 +1,7 @@
 package SD_Project.src;
 
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 
 public class Downloader extends Thread{
     public Downloader() {
@@ -8,7 +9,47 @@ public class Downloader extends Thread{
     }
     
     private JSONObject searchURL(String url) {
+        System.out.println("Searching for: " + url);
+        JSONObject json = new JSONObject();
+
+        //Get the url information(title, description, associated urls)
+        try {
+            //Url
+            json.put("url", url);
+
+            //Title
+            Document doc = Jsoup.connect(url).get();
+            json.put("title", doc.title());
+
+            //Description
+            Elements paragraphs = doc.select("p");
+            StringBuilder textBuilder = new StringBuilder();
+
+            for (Element paragraph : paragraphs) {
+                textBuilder.append(paragraph.text()).append("\n");
+            }
+
+            String text = textBuilder.toString();
+            json.put("text", text);
+
+            //Associated Urls
+            Elements links = doc.select("a[href]");
+            JSONArray linksArray = new JSONArray();
+
+            for (Element link : links) {
+                String absoluteUrl = link.attr("abs:href");
+                linksArray.put(absoluteUrl);
+            }
+
+            json.put("links", linksArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            json.put("url", "Not available");
+
+        }
         
+        return json;
     }
     
 }
