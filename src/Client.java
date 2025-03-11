5e0f7faeaea8ced506b252ebe291ace43d8cdfff
yasminswerak <yasmin.swerak@gmail.com>
2025-03-11 14:23:22 +0000

    começo client

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,32 @@
 package SD_Project.src;
+import java.util.Scanner;
 
 public class Client {
+    private void menu(){
+        System.out.println(
+        "1. pesquisar\n"+
+        "2. index\n"+
+        "3. admin page\n"+
+        "4. sair\n");
+    linha=sc.nextLine();
+    if(linha.equals("4")){
+        System.out.println("Saindo...");
+        break;
+    }else if(linha.equals("3")){
+    }
+    else if(linha.equals("2")){
+        System.out.println("URL a indexar: ");
+        String url = sc.nextLine();
+        try{
+            // adicionar a queue 
+        }catch (Exception e) {
+            System.out.println("Exception indexing: " + e);
+        }
+        System.out.printl("URL indexado");
+
+    }else if(linha.equals("1")){
+        
+    }
+    }
     
 }
