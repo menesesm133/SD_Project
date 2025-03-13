5ccfe9ba6b9bc544ec96c24932b2707a12e80ebc
Miguel Meneses <menesesm133@gmail.com>
2025-03-13 16:21:31 +0000

    Read RMI requirements from the properties file

--- src/Downloader.java
+++ src/Downloader.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -19,6 +20,30 @@ public class Downloader extends Thread{
      */
     public Downloader() {
         super("Server: " + (long) (Math.random() * 1000));
+    }
+
+    public void run() {
+        String RMI_ADDRESS;
+        int RMI_PORT;
+
+        //Read information regarding the RMI from "properties.txt"
+        try(BufferedReader br = new BufferedReader(new FileReader(new File("urls.txt")))){
+            String line;
+
+            while((line = br.readLine()) != null) {
+                String[] token = line.split(":");
+
+                if (token[0].trim().equals("RMI Address")) {
+                    RMI_ADDRESS = token[1].trim();
+                }
+                
+                if (token[0].trim().equals("RMI Port")) {
+                    RMI_PORT = Integer.parseInt(token[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     /**
