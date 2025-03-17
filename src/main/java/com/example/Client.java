ab30fd0a40e00002de96c9c720b36ade5b6bec8e
Miguel Meneses <menesesm133@gmail.com>
2025-03-17 14:37:26 +0000

    Changed somethings in the client(main)

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -1,18 +1,26 @@
 package com.example;
 
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
+/**
+ * This class is responsible for the client functionality.
+ */
 public class Client {
+    public Client() throws RemoteException {
+        super();
+    }
+
     private void menu(){
         System.out.println(
-        "1. pesquisar\n"+
-        "2. index\n"+
-        "3. admin page\n"+
-        "4. sair\n");
+        "1. Search\n" + //Asks for URL and goes to a menu with the search results
+        "2. Index URL to Queue\n" + //Adds a URL to the queue
+        "3. Administrator Page\n" + //The admin page gets the status of the system
+        "4. Exit\n"); //Ends the client
     Scanner sc = new Scanner(System.in);
     String linha = sc.nextLine();
     if(linha.equals("4")){
-        System.out.println("Saindo...");
+        System.out.println("Exiting...");
     }else if(linha.equals("3")){
     }
     else if(linha.equals("2")){
@@ -28,5 +36,19 @@ public class Client {
     }else if(linha.equals("1")){
         
     }
+    }
+
+    /**
+     * Main method of the client.
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client started");
+            Client c = new Client();
+            c.menu();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
