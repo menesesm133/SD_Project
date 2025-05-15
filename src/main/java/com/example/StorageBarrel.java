8799c3601921edfa30452a69be6df3d01f3b426e
yasminswerak <yasmin.swerak@gmail.com>
2025-05-15 14:15:29 +0100

    meta2-start

--- src/main/java/com/example/StorageBarrel.java
+++ src/main/java/com/example/StorageBarrel.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demo;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/main/java/com/example/StorageBarrelInterface.java
+++ src/main/java/com/example/StorageBarrelInterface.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demo;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
